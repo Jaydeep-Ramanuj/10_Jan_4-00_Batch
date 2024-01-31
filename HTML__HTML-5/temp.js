8e6cfc38d47b0c89b46fdd405c7373f942eedf8c
Jaydeep-Ramanuj-Tops <jaydeepramanuj.tops@gmail.com>
2024-01-31 18:21:24 +0530

    Module 2 Lecture 8 & 9

---
+++ HTML__HTML-5/temp.js
@@ -0,0 +1,12 @@
+console.log('This marks as file load');
+addValue('Priyank')
+
+
+function addValue(name){
+    console.log('debug');
+    document.getElementById('input2').value=name
+    }
+        function sum(para1, para2){
+            console.log('This will also print');
+            console.log(para1+para2);
+        }
